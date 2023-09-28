10900dfc6fa6004cdcd60a19b4c50df6f1396d34
Alex <leha.pavlov.03.03@mail.ru>
2023-09-28 15:38:26 +0700

    step 1

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+int Point1X = 20;
+int Point1Y = 1;
+int Point2X = 1;
+int Point2Y = 30;
+int Point3X = 40;
+int Point3Y = 30;
+
+Console.SetCursorPosition(Point1X, Point1Y);
+Console.WriteLine("+");
+Console.SetCursorPosition(Point2X, Point2Y);
+Console.WriteLine("+");
+Console.SetCursorPosition(Point3X, Point3Y);
+Console.WriteLine("+");
